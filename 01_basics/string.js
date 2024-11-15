5f2d26d2236b7b188fa64210f1c64d20cf027f21
mayankmalik263 <mayankmalik263@gmail.com>
2024-11-15 23:31:00 +0530

    learned about different prototypes in strings

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -15,4 +15,17 @@ console.log(gameName);
 console.log(typeof(gameName)); // as we can see that when we define like this with "new" keyword 
 // so by using this you can see that string is defined as an object which you can see in the console tab of any website
 // object --> means ki it will be generated with a key:value pair + length of the string + prototype methods + primitive value
-// to MASTER STRING MASTER all the prototype methods 
+// to MASTER STRING MASTER all the prototype methods
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('y'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-5, 4);
+console.log(anotherString);
