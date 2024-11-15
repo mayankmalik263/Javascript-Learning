2cb625674f1d26030f42700c29c7d492baffa8ea
mayankmalik263 <mayankmalik263@gmail.com>
2024-11-15 22:40:18 +0530

    learned about string interpolation

---
+++ 01_basics/string.js
@@ -0,0 +1,10 @@
+const name = "Mayank"
+const RepoCount = 60
+
+// console.log(name + RepoCount + " Value");
+
+// Outdated ho chuka h ye ab koi use nhi krta isse
+
+console.log(`Hello my name is ${name} and my repo count is ${RepoCount}`);
+// String interpolation (${}) you can write any variable in those curly brackets to presented at that place
+// ${} here strings will be injected that you want to display
