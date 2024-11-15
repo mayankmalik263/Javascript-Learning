8fb2969fceba6872a944474d1c41a2d920682962
mayankmalik263 <mayankmalik263@gmail.com>
2024-11-15 22:49:22 +0530

    learned about declaring string "new" keyword and what changes it makes

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -8,3 +8,11 @@ const RepoCount = 60
 console.log(`Hello my name is ${name} and my repo count is ${RepoCount}`);
 // String interpolation (${}) you can write any variable in those curly brackets to presented at that place
 // ${} here strings will be injected that you want to display
+
+const gameName = new String('Mayank');
+
+console.log(gameName);
+console.log(typeof(gameName)); // as we can see that when we define like this with "new" keyword 
+// so by using this you can see that string is defined as an object which you can see in the console tab of any website
+// object --> means ki it will be generated with a key:value pair + length of the string + prototype methods + primitive value
+// to MASTER STRING MASTER all the prototype methods 
