7130dc77ef0fe87b00c496296a26ccdf16d541c9
mayankmalik263 <mayankmalik263@gmail.com>
2024-11-17 22:42:29 +0530

    Just added some comments to not break off the streak as i am sick today wasn't able to learn much just revised the old concepts + there are assignments too!

--- 01_basics/nums_math.js
+++ 01_basics/nums_math.js
@@ -1,13 +1,13 @@
 const score = 400
 console.log(score);
  
-const balance = new Number(100);
+const balance = new Number(100); // this new keyword will make sure that ki "100" is defined as a number
 console.log(balance);
 
-console.log(balance.toString());
-console.log(typeof(balance.toString()));
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+console.log(balance.toString()); //this prototype will change the number to a string
+console.log(typeof(balance.toString())); // checking the type of '100'
+console.log(balance.toString().length); // and as number is now converted to a string we can check it's length
+console.log(balance.toFixed(2)); // toFixed will give 2 digits after the decimal which we helps for the simplicity of the number
  
  
  
